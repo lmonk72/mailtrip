258bf182f83eddb3e2fcc4f1a906dbd0a84742fb
lmonk72 <lmonk72@gmail.com>
2021-07-28 23:21:32 +0100

    Better workflow, but needs refactoring

--- README.md
+++ README.md
@@ -1,22 +1,31 @@
 # Mailtrip
-A simple bundler to help beat HTML email inconsistencies 👍.
+A simple bundler to help make email development less inconsistent, and more efficient 👍.
+
+## The problem
+Have you ever been asked to develop a HTML email(s)? If so, you will know what a nuisance it can be. It is like being caught in a time warp...
+
+While web development has received a lot of much needed ❤️ and support (which is awesome) - email development has drastically lagged behind. While there has been some progress, email development remains wholly dependant on the dated table-based approaches of old.
+
+The difference is staggering, and it needs to be addressed.
 
 ## What is this project is trying to acheive?
-HTML emails have been living in the early 2000’s for a long time and while web development has surged ahead, email development has drastically lagged behind. Email development remains dependant on the dated table-based approaches of old to get the most performant result across the greatest number of platforms and devices 📱. 
 
-Emails ✉️ currently remain the **#1** form of communication between businesses and their audience. What we have here, is a recipe for disaster. Modern syntax, strong semantics, component-driven development workflows and UX are often relegated to the side-lines 👀.
+Like many other web developers, I found email development to be slow, inefficient, frustrating, and limited. Kiss goodbye to neat workflows, semantics and more.
 
-While this tool cannot fix most of that, it is designed to give you a flexible component-oriented 'bundle'. Think Tailwind CSS meets HTML tables.
+While no single tool can remedy the out-of-date requirements, this 'bundle' aims to bridge some of the workflow issues, and puts a web development flavour on the table.
 
-## What can this 'bundle' offer
+## What does this 'bundle' offer
 Out of the box, you can:
 
 - Write SASS/SCSS and have it compile to CSS
-- Purge unused CSS to remove bloat
-- Compress images
-- Inline CSS
-- Minify your HTML
-
+- Generate multiple classes based on a configuration file
+- Purge unused CSS (to remove bloat)
+- Compress your images
+- Inline your CSS into the HTML
+- Minify CSS
+- Minify HTML
+- Develop with a real-time output (via Browser-Sync)
+- Output all cleaned and compressed files into a neat package (for easy asset sharing)
 
 ## Future aims
 At the moment, this project is in its infancy, and, as such is likely to need several adjustments before being considered production-ready.
@@ -29,7 +38,15 @@ I am aiming to get the bundle to ship:
 ## What has this bundle been tested with?
 To make sure the output is working as intended, it has gone through [W3C Markup Validation](https://validator.w3.org/#validate_by_input), referenced ['Can I Email'](https://caniemail.com) for email client support, and been tested in [Litmus's email preview tool](https://www.litmus.com/pre-send-testing/) to check rendering consistency.
 
-## Known issues
-- [ ] Media queries are not merging into HTML
-- [ ] Need to remove redundant CSS `<link>` in HTML after compression
-- [ ] Need to see if there's a way to remove unused CSS files once concatenated together
+## Reference Materials 
+New to email development? Here are a few resources which you might find helpful:
+- ['How to Target Email Clients'](https://howtotarget.email/) - a quick way to identify ways of styling specific email clients
+- ['Can I Email'](https://caniemail.com) - a comprehensive guide to HTML and CSS email client support
+
+---
+## Getting Started
+To get up and running as quickly as possible, clone/download this project. 
+
+Once the project is downloaded, run: `npm install`, to install *Gulp* (Version 4) and all of the dependencies needed. 
+
+Because this process is using the task-runner 'Gulp', you can choose to change the configuration to suit your requirements. E.g. Swap gulp-clean-css for PostCSS and CSSNano and so on.
--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,69 @@
-const { src, dest, watch, series } = require('gulp');
+const {
+  src,
+  dest,
+  watch,
+  series
+} = require('gulp');
+
 const sass = require('gulp-sass');
 const Fiber = require('fibers');
-const purgecss = require('gulp-purgecss');
 const concat = require('gulp-concat');
-const inlineCss = require('gulp-inline-css');
-const imagemin = require('gulp-imagemin');
-const sourcemaps = require('gulp-sourcemaps');
-const browsersync = require('browser-sync').create();
+const purgecss = require('gulp-purgecss');
 const smoosher = require('gulp-smoosher');
+const stripCssComments = require('gulp-strip-css-comments');
+const cleanCSS = require('gulp-clean-css');
+const inlineCss = require('gulp-inline-css');
+const htmlmin = require('gulp-htmlmin');
+const imagemin = require('gulp-imagemin');
+const prompt = require('gulp-prompt');
+const browsersync = require('browser-sync').create();
+const rename = require("gulp-rename");
+const del = require('del');
 
 //Dart sass compiler
 sass.compiler = require('sass');
 
 //Compile SASS to CSS
 function compileCSS() {
-  return src('build/scss/**/*.scss', { sourcemaps: true } )
-    .pipe(sass({fiber: Fiber}).on('error', sass.logError))
+  return src('build/scss/**/*.scss', {
+      sourcemaps: true
+    })
+    .pipe(sass({
+      fiber: Fiber
+    }).on('error', sass.logError))
     .pipe(concat('styles.css'))
-    .pipe(purgecss({content: ['./*.html']}))
-    .pipe(dest('dist/css', { sourcemaps: '.' }))
+    .pipe(purgecss({
+      content: ['./*.html']
+    }))
+    .pipe(stripCssComments({
+      preserve: false
+    }))
+    .pipe(cleanCSS())
+    .pipe(dest('dist/css', {
+      sourcemaps: '.'
+    }))
 }
 
+//Compile a boilerplate to dodge certain css purging
 function boilerplateCSS() {
-  return src('build/scss/base/boilerplate.scss', { sourcemaps: true } )
-    .pipe(sass({fiber: Fiber}).on('error', sass.logError))
+  return src('build/scss/base/boilerplate.scss', {
+      sourcemaps: true
+    })
+    .pipe(sass({
+      fiber: Fiber
+    }).on('error', sass.logError))
+    .pipe(stripCssComments({
+      preserve: false
+    }))
     .pipe(smoosher())
+    .pipe(cleanCSS())
     .pipe(inlineCss({
-      removeStyleTags: false,
+      removeStyleTags: true,
       removeLinkTags: true,
       preserveMediaQueries: true
     }))
     .pipe(dest('dist/css'));
 }
-
-//Combine all the different CSS files together
-// function mergeCSS() {
-//   return src('dist/**/*.css')
-//     .pipe(concat('styles.css'))
-//     .pipe(dest('dist'));
-// }
-
-// //Remove unused CSS
-// function purgeCSS() {
-//   return src('build/**/*.css')
-//     .pipe(purgecss({content: ['./*.html']}))
-//     .pipe(dest('dist/'))
-// }
 
 //Merge CSS into the HTML 
 function inlineCSS() {
@@ -56,9 +74,11 @@ function inlineCSS() {
       removeLinkTags: true,
       preserveMediaQueries: true
     }))
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
     .pipe(dest('dist'));
 }
-
 
 //Compress images 
 function minifyIMG() {
@@ -91,7 +111,7 @@ function minifyIMG() {
 function browsersyncServe(callback) {
   browsersync.init({
     server: {
-        baseDir: "./dist/"
+      baseDir: "./dist/"
     }
   });
   callback();
@@ -103,10 +123,125 @@ function browsersyncReload(callback) {
   callback();
 }
 
+//Handle when a project is saveed
+function save() {
+  return src('./dist/index.html')
+    /* Prompt user for project name and confirmation of what to do next */
+    .pipe(prompt.prompt([{
+          type: 'input',
+          name: 'emailName',
+          message: 'What is the name of the email?',
+          validate: function (emailName) {
+            if (typeof (emailName) == 'undefined' || typeof (emailName) == null || emailName == '') {
+              return false;
+            }
+            return true;
+          }
+        },
+        {
+          type: 'input',
+          name: 'clientName',
+          message: 'Who is the client?',
+          validate: function (clientName) {
+            if (typeof (clientName) == 'undefined' || typeof (clientName) == null || clientName == '') {
+              return false;
+            }
+            return true;
+          }
+        },
+        {
+          type: 'checkbox',
+          name: 'nextSteps',
+          message: 'What would you like to do now?',
+          choices: ['Save project', 'Save and Export', 'Export only', 'Nothing'],
+          validate: function (nextSteps) {
+            if (typeof (nextSteps) == 'undefined' || typeof (nextSteps) == null || nextSteps == '') {
+              return false;
+            }
+            return true;
+          }
+        }
+      ],
+
+      /* Results are stored in 'results' */
+      function (results) {
+        let emailName = results.emailName;
+        let clientName = results.clientName;
+        let nextStep = results.nextSteps.toString();
+
+        switch (nextStep) {
+          case 'Save project':
+            src("build/**/*")
+              .pipe(dest(`emails/${clientName}/${emailName}/saved/build`));
+            
+            src("*.html")
+              .pipe(dest(`emails/${clientName}/${emailName}/saved`));
+
+            console.log(`The assets have been saved to 'emails/${clientName}/${emailName}/saved'.`);
+            break;
+
+          case 'Save and Export':
+            //Save
+            src("build/**/*")
+              .pipe(dest(`emails/${clientName}/${emailName}/saved/build`));
+            //Save
+            src("*.html")
+              .pipe(dest(`emails/${clientName}/${emailName}/saved`));
+            //Export
+            src("dist/**/*.html")
+              .pipe(dest(`emails/${clientName}/${emailName}/export`));
+            //Export
+            src("dist/img/**/*")
+              .pipe(dest(`emails/${clientName}/${emailName}/export/img`))
+
+            console.log(`The assets have been saved and exported to 'emails/${clientName}/${emailName}'.`);
+            break;
+
+          case 'Export only':
+
+            src("dist/**/*.html")
+              .pipe(dest(`emails/${clientName}/${emailName}/export`));
+
+            src("dist/img/**/*")
+              .pipe(dest(`emails/${clientName}/${emailName}/export/img`))
+
+              console.log(`The assets have been saved and exported to 'emails/${clientName}/${emailName}/export'.`);
+            break;
+
+          case 'Nothing':
+            console.log("OK, let's chill");
+            break;
+
+          default:
+            break;
+        }
+
+      }));
+}
+
+
 //Watch for changes
 function watchTask() {
-  watch(['*.html', 'build/scss/**/*.scss'], series(compileCSS, boilerplateCSS, inlineCSS, minifyIMG, browsersyncReload));
+  watch(['*.html', 'build/scss/**/*.scss'], series(compileCSS, boilerplateCSS, inlineCSS, browsersyncReload));
+  watch(['*', 'build/img/**/*'], series(minifyIMG, browsersyncReload));
 }
+
+async function clean() {
+  //Set a three-second delay so that any tasks before it can complete
+  setTimeout(function () {
+    return del.sync(['dist/**', '!dist']);
+  }, 3000);
+}
+
+exports.build = series(
+  compileCSS,
+  boilerplateCSS,
+  inlineCSS,
+  minifyIMG
+);
+
+exports.save = series(save);
+exports.new = series(clean);
 
 //Default Gulp tasks
 exports.default = series(
@@ -117,3 +252,6 @@ exports.default = series(
   browsersyncServe,
   watchTask
 )
+
+//Configure any individual tasks 
+//exports.taskName = GulpTaskName
